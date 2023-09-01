15e60d13e4d87ecbfa224bcf6097789597ca4cc8
mzunairtariq <contact.zunair@gmail.com>
2023-09-01 11:33:23 +0500

    Footer and Testimonials enlisted!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
+import Testimonial from "./Components/Testimonial/Testimonial";
+import Footer from "./Components/Footer/Footer";
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <Testimonial />
+      <Footer />
     </div>
   );
 }
